d2323f0aa582642e1760e68b8ce9e54c39397e4c
Abhi Pathak <abhipathakorg@gmail.com>
2025-04-28 23:46:39 +0530

    Modified Pallindrome Header file

---
+++ pallindrome_modified_abhi.h
@@ -0,0 +1,13 @@
+// pallindrome_abhi.h
+
+#ifndef PALLINDROME_ABHI_H
+#define PALLINDROME_ABHI_H
+
+typedef int (*PalindromeFunc)(int);
+
+int isPalindrome(int n);
+
+PalindromeFunc getPalindromeChecker();
+
+#endif 
+
